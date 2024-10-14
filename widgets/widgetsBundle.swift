ef94e8ea5b6f6c51c99401bc66cd0df6b76bb8d4
Arron-Stothart <arron@mercurylabs.io>
2024-10-14 15:39:02 +0100

    refactor: Add widgets target + rename existing schemes

---
+++ widgets/widgetsBundle.swift
@@ -0,0 +1,17 @@
+//
+//  widgetsBundle.swift
+//  widgets
+//
+//  Created by Arron Stothart on 14/10/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widgetsBundle: WidgetBundle {
+    var body: some Widget {
+        widgets()
+        widgetsLiveActivity()
+    }
+}
